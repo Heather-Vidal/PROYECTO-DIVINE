ddf6a6e368c2c1824593a73716f8d1bfc9d5f693
heather <vidalnunezheather@pedropoveda.edu.bo>
2026-06-09 10:41:32 -0400

    NUEVA BD

--- SCRIPTBD.sql
+++ SCRIPTBD.sql
@@ -42,6 +42,43 @@ CREATE TABLE IF NOT EXISTS `DIVINE`.`PRODUCTO` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `DIVINE`.`PEDIDOS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DIVINE`.`PEDIDOS` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `fecha` DATE NULL,
+  `estado` VARCHAR(45) NULL,
+  `nombrevendedor` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `DIVINE`.`CARRITO`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `DIVINE`.`CARRITO` (
+  `PRODUCTO_codigo` INT NOT NULL,
+  `PEDIDOS_ID` INT NOT NULL,
+  `cantidad` INT NULL,
+  `costototal` DOUBLE NULL,
+  PRIMARY KEY (`PRODUCTO_codigo`, `PEDIDOS_ID`),
+  INDEX `fk_PRODUCTO_has_PEDIDOS_PEDIDOS1_idx` (`PEDIDOS_ID` ASC) ,
+  INDEX `fk_PRODUCTO_has_PEDIDOS_PRODUCTO_idx` (`PRODUCTO_codigo` ASC) ,
+  CONSTRAINT `fk_PRODUCTO_has_PEDIDOS_PRODUCTO`
+    FOREIGN KEY (`PRODUCTO_codigo`)
+    REFERENCES `DIVINE`.`PRODUCTO` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PRODUCTO_has_PEDIDOS_PEDIDOS1`
+    FOREIGN KEY (`PEDIDOS_ID`)
+    REFERENCES `DIVINE`.`PEDIDOS` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
