b9568a87dd68bb83031a0ef6e16fd3c49e60357d
Heather-Vidal <vidalnunezheather@pedropoveda.edu.bo>
2026-06-12 15:55:27 -0400

    sesion

--- CRUD-producto/readtodoprodu.php
+++ CRUD-producto/readtodoprodu.php
@@ -195,7 +195,6 @@ while($fila=$resultado->fetch_assoc()){
 ?>
     </div>
     <div class="volver">
-      <a href="totu.php">⬅ Volver al inicio</a>
       <a href="../totu.php">⬅ Volver al inicio</a>
     </div>
   </div>
