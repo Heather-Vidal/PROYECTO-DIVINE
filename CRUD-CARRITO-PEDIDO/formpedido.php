4c85057c6550201adb8264fc994730f86d01b322
hagointrosgratis12-design <hagointrosgratis12@gmail.com>
2026-06-17 12:26:50 -0400

    crud pedido carrito

---
+++ CRUD-CARRITO-PEDIDO/formpedido.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+    $vendedor=$_SESSION['Nombre'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nuevo Pedido</title>
+</head>
+<body>
+
+<h2>AGREGA EL PRODUCTO</h2>
+
+<form action="createpedido.php" method="POST">
+
+    Nombre:
+    <input type="text" name="nombre" ><br><br>
+
+    Fecha:
+    <input type="date" name="fecha" ><br><br>
+
+    <input type="hidden" name="estado" value="En Proceso">
+
+    Nombre Vendedor:
+    <input type="text" name="nombreVendedor" value="<?php echo $vendedor?>" readonly><br><br>
+
+    <input type="submit" value="Nuevo Pedido">
+
+</form>
+
+</body>
+</html>
