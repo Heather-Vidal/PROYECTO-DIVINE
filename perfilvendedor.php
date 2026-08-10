6c257a735d445375e409083a51f59d52dbdf74b3
sivila aileen <sivilacaeroaileen@pedropoveda.edu.bo>
2026-08-10 14:24:35 -0400

    Update perfilvendedor.php

--- perfilvendedor.php
+++ perfilvendedor.php
@@ -476,7 +476,7 @@ pointer-events:none;
 
 <div class="profile-header">
 <?php echo $inicial; ?>
-<img src="../imagenes/vendee.png" alt="Perfil">
+<img src="./imagenes/vendee.png" alt="Perfil">
 
 <h2> <?php echo $_SESSION['nombre']?> </h2>
 
