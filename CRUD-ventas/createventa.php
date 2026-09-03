806649ffab3c6ae6e3fd49c75e0724b781638bfc
aileen sivila <sivilacaeroaileen@pedropoveda.edu.bo>
2026-09-03 14:47:36 -0400

    subi tus commits bro

--- CRUD-ventas/createventa.php
+++ CRUD-ventas/createventa.php
@@ -358,3 +358,17 @@ try {
 $conn->close();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</head>
+<body>
+    
+</body>
+</html>
